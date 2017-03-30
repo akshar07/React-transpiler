3805ed0653160bb3be5c46b4630ebc2bd0a50981
akshar07 <takleakshar@gmail.com>
2017-03-30 15:11:57 -0700

    react transpiler

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+class App extends React.Component {
+constructor(){
+  super()
+    this.state={
+      input:'/* add your jsx here*/',
+      output:'',
+      err:''
+    }
+  }
+  update(e){
+    let code=e.target.value;
+    try{
+      this.setState({
+        output:window.Babel
+          .transform(code,{presets:['es2015','react']})
+            .code,
+        err:''
+      })
+    }
+    catch(err){
+      this.setState({
+        err:err.message
+      })
+    }
+  }
+render(){
+  return(
+    <div>
+      <header>{this.state.err}</header>
+      <div className="container">
+        <textarea
+          onChange={this.update.bind(this)}
+          defaultValue={this.state.input}>
+        </textarea>
+        <pre>
+          {this.state.output}
+        </pre>
+      </div>
+    </div>
+  )
+}
+}
+
+
+
+
+
+
+export default App
